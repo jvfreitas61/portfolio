d88b64d768302f499917374e97a7dbd081abeeda
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2025-08-02 02:37:22 -0300

    update_02-08

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function enviarWhats(event) {
   event.preventDefault();
   const nome = document.getElementById('nome').value;
   const mensagem = document.getElementById('mensagem').value;
-  const telefone = '5561998599052';
+  const telefone = '5561995925636';
 
   const texto = `Olá, me chamo ${nome}. ${mensagem}`;
   const msgFormatada = encodeURIComponent(texto);
